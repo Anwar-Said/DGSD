447651e9038176b2da49bf915f172b401ca90869
Anwar Said <40773404+Anwar-Said@users.noreply.github.com>
2021-02-17 17:39:41 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ except ImportError:
 
 setup(
     name='dgsd',
-    version='v1.0.0',
+    version='v1.0.1',
     author='Anwar Said',
     author_email='anwar.said@itu.edu.pk',
     description='DGSD: Distributed Graph Representation via Graph Statistical Properties!',
     long_description='long_description',
     packages=['dgsd',],
     url='https://github.com/Anwar-Said/DGSD',
-    download_url = 'https://github.com/Anwar-Said/DGSD/archive/v1.0.0.tar.gz',
+    download_url = 'https://github.com/Anwar-Said/DGSD/archive/v1.0.1.tar.gz',
     license='MIT',
     install_requires=[
         'numpy',
